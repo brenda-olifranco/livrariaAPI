f9ba50e6208080919cb69c0c813599534b0a7dcb
Brenda Franco <brenda_olifranco@hotmail.com>
2025-07-09 19:50:42 -0300

    Adicionei controllers e rotas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import conectaNaDataBase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDataBase();
 
 const app = express();
-app.use(express.json()) //middleware
+routes(app);
 
 conexao.on("error", (erro) =>{
     console.error("erro de conexão", erro)
@@ -15,17 +15,7 @@ conexao.once("open",()=>{
     console.log("Conexão com o banco feita com sucesso")
 })
 
-//Um middleware é uma função que roda no meio do caminho entre a requisição do cliente e a resposta do servidor. Toda vez que chega um dado como body ele chega como string, por isso temos que converter para json pelo middleware
 
-
-app.get("/", (req, res)=>{
-    res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", async (req, res)=>{
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-});
 
 app.get("/testando",(req, res)=>{
     res.status(200).send("Testando")
@@ -36,10 +26,10 @@ app.get("/livros/:id", (req, res) => {
     res.status(200).json(livros[index])
 })
 
-app.post("/livros",(req, res)=>{
+/* app.post("/livros",(req, res)=>{
     livros.push(req.body); //o express cria o objeto req e dentro dele está a corpo da requisição
     res.status(201).send("Livro cadastrado com sucesso")
-})
+}) */
 
 app.put("/livros/:id",(req, res) =>{
     const index = buscaLivro(req.params.id);
